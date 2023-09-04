ee8a02be434050adb1eac1beca5cf77a9195a3f4
root <root@DESKTOP-KKU56CK>
2023-09-04 13:04:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList';
+import SelectedContact from './components/SelectedContact';
+import './App.css'
+
+
+function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+  return (
+    <>
+    {selectedContactId ?
+        (<SelectedContact selectedContactId={selectedContactId} setSelectedContactId={setSelectedContactId} />)
+        : (<ContactList setSelectedContactId={setSelectedContactId} />)}
+    </>
+  )
+}
+  
+export default App
